bcb29ef08254f221627e4245632d43034a7daea1
John Wohler <johndoubleyou11@gmail.com>
2023-01-25 09:37:56 -0400

    Initial commit

---
+++ MovingPlayer/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovingPlayer
+{
+    internal class Player
+    {
+        private int x = 10;
+        private int y = 10;
+        public bool gameOver = false;
+
+        public void Update()
+        {
+            ConsoleKeyInfo input;
+            input = Console.ReadKey(true);
+
+            if (input.Key == ConsoleKey.W) y -= 1;
+            if (input.Key == ConsoleKey.S) y += 1;
+            if (input.Key == ConsoleKey.A) x -= 1;
+            if (input.Key == ConsoleKey.D) x += 1;
+
+            if (input.Key == ConsoleKey.Escape) gameOver = true;
+
+        }
+
+        public void Draw()
+        {
+            Console.Clear();
+
+            Console.SetCursorPosition(x, y);
+            Console.Write("+");
+        }
+    }
+}
---
+++ MovingPlayer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovingPlayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            Console.CursorVisible = false;
+
+            while (player.gameOver==false)
+            {
+
+                player.Update();
+                player.Draw();
+
+            }
+        }
+    }
+}
